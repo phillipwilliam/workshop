002b956f5de7713e59d56e35b153e7f24171fc97
Phillip William <phillipwilliam@gmail.com>
2019-11-07 10:35:10 +1100

    Connect to store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Content } from './components/Content';
+import { Provider } from 'react-redux';
+import { Content } from './containers/Content';
+import { store } from './redux/store';
 import './css/base.css';
 
 ReactDOM.render(
-    <Content />,
+    <Provider store={store}>
+        <Content />
+    </Provider>,
     document.getElementById('root')
 );
